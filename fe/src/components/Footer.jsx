3795c0d49c9908e1a5e4bfb4816574d11c7b36f8
pedrofranzina <pedrofranzinaa@gmail.com>
2025-08-14 23:15:07 +0100

    hhf

--- fe/src/components/Footer.jsx
+++ fe/src/components/Footer.jsx
@@ -1,8 +1,29 @@
 const Footer = () => {
     return (
-<footer>
-
-</footer>
+        <footer className="bg-[#503B31] h-[200px]">
+            <div className="flex justify-between lg:justify-around mx-auto h-full items-center px-4">
+                <div className="flex flex-col items-center">
+                    <div className="flex flex-col md:flex-row gap-4">
+                        <a
+                            href="https://www.instagram.com/pedrofranzina"
+                        >
+                            <img className="w-6 h-6" src="/img/instagram.png" alt="Instagram Icon" />
+                        </a>
+                        <a href="https://www.linkedin.com/in/pedrofranzina/">
+                            <img className="w-6 h-6" src="/img/linkedin.png" alt="LinkedIn Icon" />
+                        </a>
+                        <a href="https://www.facebook.com/pedrofranzina">
+                            <img className="w-6 h-6" src="/img/facebook.png" alt="Facebook Icon" />
+                        </a>
+                        <a href="https://www.github.com/pedrofranzina">
+                            <img className="w-6 h-6" src="/img/github.png" alt="GitHub Icon" />
+                        </a>
+                    </div>
+                    <div className="mt-4 flex space-x-4">
+                    </div>
+                </div>
+            </div>
+        </footer>
     );
 }
 export default Footer;
---
+++ fe/src/views/Homepage.jsx
@@ -0,0 +1,20 @@
+const Homepage = () => {
+    return (
+        <div className="min-h-screen bg-[#705D56]">
+            <div className="container mx-auto px-4 py-8">
+                <div className="text-center mb-12">
+                    <h1 className="text-4xl font-bold text-[#fff] mb-4">
+                        Welcome to the CLOSEIN
+                    </h1>
+                    <p className="text-xl text-gray-600 mb-8">
+                        Where are your clothes?
+                    </p>
+                    <div className="max-w-2xl mx-auto">
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Homepage;
